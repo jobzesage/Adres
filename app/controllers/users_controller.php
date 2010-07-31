2b4889adb79df3e3cc86ae028e995f14324aa117
Rajib Ahmed <l.rajibahmed@gmail.com>
2010-07-31 16:45:34 +0600

    Bug#59 css file edited for large number of columns

--- app/controllers/users_controller.php
+++ app/controllers/users_controller.php
@@ -503,7 +503,6 @@ class UsersController extends AppController {
 		
 		$fields   = $this->Field->getPluginTypes($contact_type_id,$hidden_fields);
 		
-		FireCake::fb($fields);	
 		# query optimization
 		$hidden_fields_list = !empty($hidden_fields) ? $this->Field->getList($hidden_fields): array();
 		
