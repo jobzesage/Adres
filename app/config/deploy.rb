8952ded6d1e625720b95109cd4b8547bacf7f277
Rajib Ahmed <l.rajibahmed@gmail.com>
2011-02-06 17:59:34 +0100

    added capcake a capistrano clone for cakephp not yet tested

---
+++ app/config/deploy.rb
@@ -0,0 +1,29 @@
+require 'rubygems'
+require 'capcake'
+
+set :application, "ADres"
+set :repository,  "git://github.com/jobzesage/Adres.git"
+set :sudo , false
+set :branch, "master"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "adres.d32.com.bd"                          # Your HTTP server, Apache/etc
+role :app, "adres.d32.com.bd"                          # This may be the same as your `Web` server
+role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+role :db,  "your slave db-server here"
+
+# If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
+
+capcake
