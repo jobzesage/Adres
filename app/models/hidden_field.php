87d9dd1746ed84995fca85f70b082fb4e28f8caf
Rajib Ahmed <l.rajibahmed@gmail.com>
2011-02-06 18:37:32 +0100

    refactoring

--- app/models/hidden_field.php
+++ app/models/hidden_field.php
@@ -2,7 +2,11 @@
 class HiddenField extends AppModel {
 	public $actsAs = array('Containable');	
 	public $name = 'HiddenField';
-	public $belongsTo = array('User' => array('className' => 'User', 'foreignKey' => 'user_id'));
+        public $belongsTo = array(
+          'User' => array(
+            'className' => 'User', 
+            'foreignKey' => 'user_id'
+        ));
 	 	
 }
 ?>
